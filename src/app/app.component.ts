fc4683975ec34965d1708b5233585f4c33e905cd
Yahi09 <valenciayahaira262@gmail.com>
2020-07-27 13:34:59 -0500

    v1 diseño de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login';
+  lg_email:string;
+  lg_pass:string;
+  r_email:string;
+  r_pass:string;
+  r_c_pass:string;
+
+  constructor(private snackBar:MatSnackBar){
+
+  }
+
+  register(){
+
+  }
+  login(){
+    if(this.lg_email=="admin"&&this.lg_pass=="admin"){
+      this.snackBar.open('Bienvenido','',{duration:1000})
+    }else{
+      this.snackBar.open('Error, favor de verificar los d','',{duration:1000})
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';//agrega el cuadro
+import {MatTabsModule} from '@angular/material/tabs';//nav de arriba
+import {MatInputModule} from '@angular/material/input';//cajas de txt
+import {MatButtonModule} from '@angular/material/button';//boton
+import {FormsModule} from '@angular/forms';//modulo de formularios
+import {MatIconModule} from '@angular/material/icon' //iconos
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule, MatTabsModule, MatInputModule,
+    MatButtonModule, FormsModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
